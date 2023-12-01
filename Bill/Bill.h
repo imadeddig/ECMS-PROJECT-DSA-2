8a489b0a8a6f1c893e9196058d7702e94e490725
Mazrou-Rayane <125483201+Mazrou-Rayane@users.noreply.github.com>
2023-12-01 17:02:39 +0100

    Add files via upload

---
+++ Bill/Bill.h
@@ -0,0 +1,30 @@
+#ifndef BILL_H
+#define BILL_H
+#include"Customer.h"
+class Bill
+{
+private:
+    double EnergyConsumption;
+    double EnergyInjection;
+    double PriceTotalConsumption;
+    double PriceTotalInjection;
+    double TotalDifference;
+    void setEnergyConsumption(const double &);
+    void setEnergyInjection(const double &);
+    void setTotalDifference();
+
+public:
+    Bill(const double&, const double&);
+    void setBill(const double &, const double &);
+    void getBill( double &,  double &,  double &,  double &,  double &);
+    double getEnergyInjection();
+    double getEnergyConsumption();
+    double getPriceTotalConsumption();
+    double getPriceTotalInjection();
+    double getTotalDifference();
+    void print();
+};
+
+
+
+#endif

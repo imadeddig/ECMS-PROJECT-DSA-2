5ee50f8dd2c13bb750c7dba170a49d1feb6641ed
yaniame <131762815+yaniame@users.noreply.github.com>
2023-12-01 19:48:31 +0100

    Update dipartement.cpp

--- dipartement.cpp
+++ dipartement.cpp
@@ -1,78 +1,27 @@
-#include "dipartement.h"
-#include "company.h"
-#include <iostream>
 
-using namespace std;
 
-int dipartement::departementid = 0;
+#include"company.h"
 
-dipartement::dipartement()
+#include<string>
+
+class dipartement
 {
-    IDdepartement= departementid ;
-    departementid++ ;
-}
+private:
+   int budget = 1000000 ;
+   int IDdepartement ;
+   string namedipartement ;
+   
 
-int dipartement::gettotalpaid()
-{
-    return 0;
-}
+       public : dipartement();
+   int gettotalpaid();
+   int gettotalpaid(int beganyear, int endmouth);
+   int gettotalpaid(int beganyear, int beganmouth, int endyear, int endmouth);
+   void setbudget(int);
+   int getbudget();
+   void setiddepartement();
+   void setnamedep(string a);
+   string getnamedep();
+   int getiddepartement();
 
-int dipartement::gettotalpaid(int beganyear, int endmouth)
-{
-    return 0;
-}
-
-int dipartement::gettotalpaid(int beganyear, int beganmouth, int endyear, int endmouth)
-{
-    return 0;
-}
-
-inline void dipartement::setbudget( int place )
-{
-
-    if (place ==1)
-    {
-             budget+=budget*0.15;
-
-}    
-
-else  if ( place <= 10 && place > 1)
-{
-             budget += budget *0.1 ;
-}
-
-else if (place == alldepartement.size())
-{
-             budget -= budget * 0.15;
-           }
-           else if (place < alldepartement.size() && place >= alldepartement.size()-10)
-           {
-            budget -= budget *0.1 ;
-           }
-           
-
-
-
-
-}
-
-inline int dipartement::getbudget()
-{
-    return 0;
-}
-
-inline void dipartement::setiddepartement()
-{
-
-
-}
-
-inline int dipartement::getiddepartement()
-{
-    return 0;
-}
-
-
-dipartement::~dipartement()
-{
-}
+   ~dipartement();
+};

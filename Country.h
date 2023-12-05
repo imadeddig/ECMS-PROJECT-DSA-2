93c072bc4b0966b0fe411c4afbac5ac85fb35564
billel khiri <145929851+bi2lelkhr@users.noreply.github.com>
2023-12-05 17:35:11 +0100

    Update Country.h

--- Country.h
+++ Country.h
@@ -1,21 +1,24 @@
 #ifndef COUNTRY_H
 #define COUNTRY_H
 #include "Region.h"
+#include "Customer.h"
 #include <iostream>
 #include <vector>
 using namespace std;
-class Country{
-    private:
-    string countryName;
-    int countryID;
-    vector <Region> regions;
-    public:
-    //open a region file, regions id should be "countryid-regionid", search for regions that has the same countryID as this instance, if so, call region constructor and insert the new object into the region vector
-    Country(string,int);
-    string getCountryName();
-    int getCountryID();
-    vector<Region> getCountryRegions();
 
-    Customer getCountryWinner();
+class Country
+{
+private:
+    string countryName;
+    string countryID;
+    vector<Region> regions;
+
+public:
+    // open a region file, regions id should be "countryid-regionid", search for regions that has the same countryID as this instance, if so, call region constructor and insert the new object into the region vector
+    Country(string, string);
+    string getCountryName();
+    string getCountryID();
+    vector<Region> getCountryRegions();
+   
 };
 #endif

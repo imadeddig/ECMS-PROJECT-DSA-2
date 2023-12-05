d04f6035b6dd4ea81cc4c9c3764d39ae618373f1
yaniame <131762815+yaniame@users.noreply.github.com>
2023-12-05 23:10:06 +0100

    Add files via upload

---
+++ Company.cpp
@@ -0,0 +1,83 @@
+#include "Company.h"
+#include "Country.h"
+
+Company::Company()
+{
+
+    ifstream countrie;
+    countrie.open("countrie.text");
+    if (!countrie)
+    {
+        cerr << "File could not be open" << endl;
+        exit(1);
+    }
+    string line;
+    getline(countrie, line);
+    while (getline(countrie, line))
+    {
+        string countrieid = line.substr(0, line.find_first_of(','));
+
+        string countriename = line.substr(line.find_first_of(',') + 1, line.length() - 1);
+        countries.push_back(District(countrieid, countriename));
+    }
+    countrie.close();
+    
+    fstream departementorder;
+    departementorder.open("departementorder.text") if (!departementorder)
+    {
+        cerr << "change the path name of your departement file to departement.text " exit(1);
+    }
+
+    string line;
+    // the departement are store id,name,budget
+    getline(departementorder, line);
+    while (getline(departementorder, line);)
+    {
+        vector<Department> a;
+        do
+        {
+
+            
+            // i twill be like that dep1id,DepartmentName,ped1budget,performance , dep2  ;
+
+            string depid = line.substr(0, line.find_first_of(','));
+            line = line.substr(line.find_first_of(','));
+            string depname = line.substr(0, line.find_first_of(','));
+            line = line.substr(line.find_first_of(','));
+            string depbudget = line.substr(0, line.find_first_of(','));
+            line = line.substr(line.find_first_of(','));
+            string depperformance = line.substr(0, line.find_first_of(','));
+            line = line.substr(line.find_first_of(','));
+           
+           
+
+            a.push_back( Department(depid, depname, stoi(depbudget)));
+
+
+        } while (line !='/n');
+
+        departmentsStored.push_back(a);
+       
+        }
+}
+
+
+
+
+
+
+
+
+/*
+        int numberdep = 0;
+
+        for (size_t i = 0; i < countries.size(); i++)
+
+        {
+            for (size_t j = 0; j < countries.at(i).regions.size(); j++)
+            {
+
+                numberdep += countries.at(i).regions.at(j).City.size();
+            }
+        }
+    */

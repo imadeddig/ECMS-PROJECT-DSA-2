1e9dc00758a10f1250453dcad24f3d818b78e27f
yaniame <131762815+yaniame@users.noreply.github.com>
2023-12-05 17:41:25 +0100

    Update City.cpp

--- city.cpp
+++ city.cpp
@@ -1,52 +1,97 @@
-#include"city.h"
+#include "City.h"
+#include "District.h"
 #include <iostream>
+#include<fstream>
+#include<string>
+#include "Customer"
 
-using namespace std ;
+#include <vector>
 
+using namespace std;
 
-
-city::city(string name , int id)
+City::City(string name, string id)
 {
-    setnamecity(name);
-   
-}
+    setcityname(name);
+    cityID = id;
 
-inline void city::setnamecity(string name, )
-{
-    if (name!='')
+    ifstream districtsFile;
+    districtsFile.open("districts.text");
+    if (!districtsFile)
     {
-        cityname =name ;
+        cerr << "File could not be open" << endl;
+        exit(1);
+    }
+    string line;
+    getline(districtsFile, line);
+    while (getline(districtsFile, line))
+    {
+        string districtid = line.substr(0, line.find_first_of(','));
+        if (districtid.substr(0, districtid.find_last_of('-')) == getCityID())
+        {
+            string cityName = line.substr(line.find_first_of(',') + 1, line.length() - 1);
+            Districts.push_back(District(districtid, districtName));
+        }
+    }
+    districtsFile.close();
+    ifstream departementfile ;
+    departementfile.open("departement.text")
+    if (!departementfile)
+    {
+        cerr << "change the path name of your departement file to departement.text "
+        exit(1);
+    }
+    string line ;
+    // the departement are store id,name,budget
+    getline(departementfile,line);
+    while (getline(departementfile, line);)
+    {
+        string depid = line.substr(0, line.find_first_of(','));
+        if (depid == cityID)
+        {
+            string name = line.substr(line.find_first_of(','), line.find_last_of(','));
+            string budg = line.substr(line.find_last_of(','),line.length()-1);
+
+            cityDepartement = Department(depid,name,budg);
+            break;
+            
+        }
+        
     }
     
+    
+
+
 }
 
-dipartement city::getdipartement()
+Department City::getDepartment()
 {
-    return dipartement;
+    return cityDepartement;
 }
 
-inline string city::getnamecity()
+void City::setcityname(string name)
 {
-    return cityname;
+    if (name !='')
+    {
+        cityname = name;
+    }
 }
 
-inline void city::setcityid(int id)
+string City::getCityName()
 {
-
-    cityid=id ;
-   
+    return cityName;
 }
 
-inline string city::getcityid()
+string City::getCityID()
 {
-    return cityid;
+    return cityID;
 }
 
-inline vector city::getdistrict()
+vector<District> City::getDistricts()
 {
-    return districts;
+    return Districts;
 }
 
-city::~city()
+City::~City()
 {
 }
+

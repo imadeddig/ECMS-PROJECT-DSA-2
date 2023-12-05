06b9cd63c28c602d700f53128db2287963e2e1af
yaniame <131762815+yaniame@users.noreply.github.com>
2023-12-05 17:39:55 +0100

    Update City.h

--- City.h
+++ City.h
@@ -1,29 +1,31 @@
 #ifndef CITY_H
 #define CITY_H
-#include<iostream>
+#include <iostream>
 #include "Department.h"
-#include "District.h"
-#include<string>
+#include "./District.h"
+#include <string>
 using namespace std;
 #include<vector>
 
 class City
 {
 private:
-string cityID;
-string cityName ;
-vector<District> Districts;
-Department cityDepartement;
+    string cityID;
+    string cityName;
+    vector<District> Districts;
+    Department cityDepartement;
+   
 
 public:
-    City(string,int);
+    City(string, string);
     Department getDepartment();
+    void setcityname(string) ;
     string getCityName();
-     int getCityID();
-    vector<District> getDistricts();   
+    string getCityID();
+    vector<District> getDistricts();
     ~City();
 
-    Customer getCityWinner();
+    
 };
 
 #endif

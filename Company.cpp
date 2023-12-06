c0ecdc263e6d71fbc01b38a0d929a45b87dbee7e
Mazrou-Rayane <125483201+Mazrou-Rayane@users.noreply.github.com>
2023-12-06 18:15:15 +0100

    Update Company.cpp

--- Company.cpp
+++ Company.cpp
@@ -10,60 +10,59 @@
 Company::Company()
 {
 
-    ifstream countrie;
-    countrie.open("countrie.text");
-    if (!countrie)
+  ifstream countrie;
+  countrie.open("countrie.text");
+  if (!countrie)
+  {
+    cerr << "File could not be open" << endl;
+    exit(1);
+  }
+  string line;
+  getline(countrie, line);
+  while (getline(countrie, line))
+  {
+    string countrieid = line.substr(0, line.find_first_of(','));
+
+    string countriename = line.substr(line.find_first_of(',') + 1, line.length() - 1);
+    countries.push_back(Country(countrieid, countriename));
+  }
+  countrie.close();
+
+  fstream departementorder;
+  departementorder.open("departementorder.text");
+  if (!departementorder)
+  {
+    cerr << "change the path name of your departement file to departement.text ";
+    exit(1);
+  }
+
+  string line;
+  // the departement are store id,name,budget
+  getline(departementorder, line);
+  while (getline(departementorder, line))
+  {
+    vector<Department> a;
+    do
     {
-        cerr << "File could not be open" << endl;
-        exit(1);
-    }
-    string line;
-    getline(countrie, line);
-    while (getline(countrie, line))
-    {
-        string countrieid = line.substr(0, line.find_first_of(','));
 
-        string countriename = line.substr(line.find_first_of(',') + 1, line.length() - 1);
-        countries.push_back(Country(countrieid, countriename));
-    }
-    countrie.close();
+      // i twill be like that dep1id,DepartmentName,ped1budget,performance , dep2  ;
 
-    fstream departementorder;
-    departementorder.open("departementorder.text");
-    if (!departementorder)
-    {
-        cerr << "change the path name of your departement file to departement.text ";
-        exit(1);
-    }
+      string depid = line.substr(0, line.find_first_of(','));
+      line = line.substr(line.find_first_of(','));
+      string depname = line.substr(0, line.find_first_of(','));
+      line = line.substr(line.find_first_of(','));
+      string depbudget = line.substr(0, line.find_first_of(','));
+      line = line.substr(line.find_first_of(','));
+      string spent = line.substr(0, line.find_first_of(','));
+      line = line.substr(line.find_first_of(','));
 
-    string line;
-    // the departement are store id,name,budget
-    getline(departementorder, line);
-    while (getline(departementorder, line))
-    {
-        vector<Department> a;
-        do
-        {
+      a.push_back(Department(depid, depname, stod(depbudget),stod(spent)));
 
-            // i twill be like that dep1id,DepartmentName,ped1budget,performance , dep2  ;
+    } while (line != "/n");
 
-            string depid = line.substr(0, line.find_first_of(','));
-            line = line.substr(line.find_first_of(','));
-            string depname = line.substr(0, line.find_first_of(','));
-            line = line.substr(line.find_first_of(','));
-            string depbudget = line.substr(0, line.find_first_of(','));
-            line = line.substr(line.find_first_of(','));
-            string depperformance = line.substr(0, line.find_first_of(','));
-            line = line.substr(line.find_first_of(','));
-
-            a.push_back(Department(depid, depname, stoi(depbudget)));
-
-        } while (line != "/n");
-
-        departmentsStored.push_back(a);
-    }
+    departmentsStored.push_back(a);
+  }
 }
-
 void Company::addCustomer(const string &name, const string &id, vector<int> ages, double amount, int day, int mouth, int year, string adress, int countryID, int regionID, int cityID, int districtID)
 {
     Customer customerMax =countries[countryID].getCountryRegions()[regionID].getRegionCities()[cityID].getDistricts()[districtID].getCustomers()->findMax();

0380a7fbeece5bb00ebe86d8a9533459d6987ade
Mazrou-Rayane <125483201+Mazrou-Rayane@users.noreply.github.com>
2023-12-10 20:30:59 +0100

    Update YearWeather.cpp

--- YearWeather.cpp
+++ YearWeather.cpp
@@ -9,47 +9,9 @@ HashWeather::HashWeather()
     yearsweather.resize(currentSize);
     for (int i = 0; i < currentSize;i++)
     {
-        yearsweather[i].yearNum = 0; //to avoid confusion
+        yearsweather[i].yearNum = 0; 
     }
-    fstream weatherFile;
-    weatherFile.open("weathers.txt");
-    string line;
-    getline(weatherFile,line);
-    while (getline(weatherFile,line))
-    {
-        string year = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-        int index = hashFunction(stoi(year));
-        if(yearsweather[index].yearNum==0)
-        {
-            yearsweather[index].yearNum = stoi(year);
-        }
-        string month = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-        string day = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-        string maxtemp = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-        string mintemp = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-        string sunnyhours = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-        string daytype = line.substr(0, line.find_first_of(","));
-        line = line.substr(line.find_first_of(",") + 1);
-
-        DayWeather dayweather(stoi(month), stoi(day), stod(maxtemp), stod(mintemp), stoi(sunnyhours), daytype);
-        yearsweather[index].days.push_back(dayweather);
-
-        if(isFull)
-
-        {
-            YearWeather a;
-            a.yearNum = 0;
-            yearsweather.push_back(a);
-        }
-    }
-
-    weatherFile.close();
+    
 }
 
 

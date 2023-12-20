ffa192aa2cf109ba5cc8ef7d86825e4d165846db
imadeddig <145927046+imadeddig@users.noreply.github.com>
2023-12-20 17:15:10 +0100

    Update Region.cpp

--- Region.cpp
+++ Region.cpp
@@ -5,16 +5,18 @@
 using namespace std;
 
 // here we have to immeditely open the file, and search for all cities that matches that region, need to be initilaized
-Region::Region(string regionName = "", string regionID = 0)
+Region::Region(string regionNam = "", string regionI = 0)
 {
+    cout<<"start Region"<<endl;
     // lazem hna neqsem id tae cities bch ymatchi li raho f region
 
+    
     // csv file reference: https://iq.opengenus.org/read-and-write-in-csv-in-cpp/#google_vignette
     //textbook of last year was also took as reference
-    regionName = regionName;
-    regionID = regionID;
+    regionName = regionNam;
+    regionID = regionI;
     ifstream citiesFile;
-    citiesFile.open("cities.csv");
+    citiesFile.open("city.txt");
     if (!citiesFile)
     {
         cerr << "File could not be created." << endl;
@@ -25,17 +27,22 @@ Region::Region(string regionName = "", string regionID = 0)
     while (getline(citiesFile, line))
     {
         string cityID = line.substr(0, line.find_first_of(','));
+        
         if (cityID.substr(0, cityID.find_last_of('-')) == regionID)
         {
-            string cityName = line.substr(line.find_first_of(',') + 1, line.length() - 1);
-            cities.push_back(City(cityID, cityName));
+          
+            string cityName = line.substr(line.find_first_of(',') + 1 );
+           
+            cities.push_back(City(cityName,cityID));
         }
     }
     citiesFile.close();
+    cout<<"end region";
 }
 
 string Region::getRegionName()
 {
+    cout<<regionName;
     return regionName;
 }
 
@@ -48,3 +55,4 @@ vector<City> Region::getRegionCities()
 {
     return cities;
 }
+

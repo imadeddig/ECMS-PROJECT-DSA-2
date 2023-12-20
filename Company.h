0912d0031602b3f43e060c2f606e78f3b892d73a
imadeddig <145927046+imadeddig@users.noreply.github.com>
2023-12-20 17:12:23 +0100

    Update Company.h

--- Company.h
+++ Company.h
@@ -1,12 +1,5 @@
-#ifndef COMPANY_H
-#define COMPANY_H
 
 #include "Country.h"
-#include "Department.h"
-#include "Company.h"
-#include "Customer.h"
-#include "Region.h"
-#include "City.h"
 #include <vector>
 #include <iostream>
 #include <chrono>
@@ -19,40 +12,50 @@ class Company
 {
 private:
     // saving the sorted departments in a hash table
-    vector<vector<Department>> departmentsStored;
+    
     vector<Country> countries;
-    Customer winner;
-  struct depart
+    
+    struct depart
     {
-        string namedep;
-        string IDdep;
-        double performence = 0;
+        Department dep ;
+        double performence =0;
     };
 
-public:
+    Customer winner;
+
+    public:
+    vector<Country> getCountry();
     Company();
     // order departments for the current year, give conditions to a specific date that departments should be inserted and sorted in that date, if not, show an error to the user
-    vector<Department> orderAlldepartement();
+    void orderAlldepartement();
     // storing the new stored department into the all sorted departmentf of the last year
-    void storeNewDepartment(vector<Department>);
-    // get performance of departments
-    void getDepartmentPerformance(int startYear = 0, int endYear = 0 , int startmonth = 0 ; int endmonth = 0);
-    // return customer with most injection
-    Customer getWinnerCustomer();
-    // printing customers bills based on user choice
-    void printBillCountry(string countryID, int yearStart = 0, int yearEnd = 0, int monthStart = 0, int monthEnd = 0);
-    void printBillRegion(string regionID, int yearStart = 0, int yearEnd = 0, int monthStart = 0, int monthEnd = 0);
-    void printBillCity(string cityID, int yearStart = 0, int yearEnd = 0, int monthStart = 0, int monthEnd = 0);
-    void printBillDistrict(string ditrictID, int yearStart = 0, int yearEnd = 0, int monthStart = 0, int monthEnd = 0);
-    ~Company();
+    
+    
 
-    // inserting a customer (additonal function) this is a whole new customer
-    void addCustomer(const string &name, vector<int> ages, double amount, int day, int mouth, int year, string adress, string districtID);
+
+    
+    void getDepartmentPerformance(int startYear , int endYear ,int,int);
+           // return customer with most injection
+    Customer getWinnerCustomer();
+   
+
+    void printBillCountry(string countryID, int yearStart , int yearEnd , int monthStart , int monthEnd );
+    void printBillRegion(string regionID, int yearStart , int yearEnd , int monthStart , int monthEnd );
+    void printBillCity(string cityID, int yearStart , int yearEnd , int monthStart , int monthEnd );
+    void printBillDistrict(string ditrictID, int yearStart , int yearEnd , int monthStart , int monthEnd );
+void callprint(string id, int yearStart , int yearEnd , int monthStart , int monthEnd );
+    ~Company();
+      
+    static bool compardep2(depart a, depart b)
+{
+  return a.performence > b.performence;
+}
+       void addCustomer(const string &name, vector<int> ages, double amount, int day, int mouth, int year, string adress, string districtID);
     void setBillFile();
     bool comparedep(Department, Department);
     void inOrderTraversal(BinaryNode *root, BinarySearchTree*,int yearStart, int yearEnd, int monthStart, int monthEnd);
-    void printBillForOneCustomer(BinarySearchTree *root, int idcust, int yearStart=0, int yearEnd=0, int monthStart=0, int monthEnd=0);
- bool compardep2(depart, depart);
+    void printBillForOneCustome(BinarySearchTree *root, int idcust, int yearStart, int yearEnd, int monthStart, int monthEnd);
+ void  printBillForOneCustomer(string givenID, int yearStart, int yearEnd, int monthStart, int monthEnd);
 };
-#endif
+
 
